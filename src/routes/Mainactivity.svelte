d759dc8e2f5c1a4b350153c750aca306bc59d289
ReD1KC <tchirkok@gmail.com>
2024-07-03 19:04:59 +0700

    Поделаль

--- src/routes/Mainactivity.svelte
+++ src/routes/Mainactivity.svelte
@@ -2,8 +2,106 @@
 
 <div class="flex-col pt-28">
 	<div class="flex items-center justify-center text-5xl leading-[64px] text-center text-white">
-		<h1 class="flex text-white font-[RHD600]">
+		<h1 class="flex text-white font-[RHD600] pb-20">
 			Main <span class="text-[orange] pl-3"> Activity</span>
 		</h1>
+	</div>
+	<div class="flex gap-24">
+		<div class="flex-col w-1/2">
+			<div
+				class="flex-col mb-10 shadow-[9px_9px_40px_13px_rgba(13,12,12,0.4),-6px_-6px_40px_5px_rgba(245,249,255,0.4)] rounded-3xl inset-[0%]"
+			>
+				<h1 class="flex text-white font-[RHD600] p-5">Frontend-developer</h1>
+				<ul class="flex-col list-disc pl-12">
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+				</ul>
+			</div>
+			<div
+				class="flex-col shadow-[9px_9px_40px_13px_rgba(13,12,12,0.4),-6px_-6px_40px_5px_rgba(245,249,255,0.4)] rounded-3xl inset-[0%]"
+			>
+				<h1 class="flex text-white font-[RHD600] p-5">Frontend-developer</h1>
+				<ul class="flex-col list-disc pl-12">
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="flex-col w-1/2">
+			<div
+				class="flex-col mb-10 shadow-[9px_9px_40px_13px_rgba(13,12,12,0.4),-6px_-6px_40px_5px_rgba(245,249,255,0.4)] rounded-3xl inset-[0%]"
+			>
+				<h1 class="flex text-white font-[RHD600] p-5">Frontend-developer</h1>
+				<ul class="flex-col list-disc pl-12">
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+				</ul>
+			</div>
+			<div
+				class="flex-col shadow-[9px_9px_40px_13px_rgba(13,12,12,0.4),-6px_-6px_40px_5px_rgba(245,249,255,0.4)] rounded-3xl inset-[0%]"
+			>
+				<h1 class="flex text-white font-[RHD600] p-5">Frontend-developer</h1>
+				<ul class="flex-col list-disc pl-12">
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+					<li class="text-white pb-4">
+						experienced in using cutting-age programming languages such as C Sharp, React, React
+						Native, Rust
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </div>
